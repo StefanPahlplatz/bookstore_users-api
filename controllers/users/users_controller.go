b433bb6383d9600f1f5aa6ea6be90eb20ef0a439
StefanPahlplatz <stefanpahlplatz@inogo.nl>
2020-04-23 19:36:39 +0200

    Lesson 31

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,6 +1,8 @@
 package users
 
 import (
+	"fmt"
+	"github.com/StefanPahlplatz/bookstore_oauth-go/oauth"
 	"github.com/StefanPahlplatz/bookstore_users-api/domain/users"
 	"github.com/StefanPahlplatz/bookstore_users-api/services"
 	"github.com/StefanPahlplatz/bookstore_users-api/utils/errors"
@@ -34,6 +36,11 @@ func Create(c *gin.Context) {
 }
 
 func Get(c *gin.Context) {
+	if err := oauth.AuthenticateRequest(c.Request); err != nil {
+		c.JSON(err.Status, err)
+		return
+	}
+
 	userId, idErr := getUserId(c.Param("user_id"))
 	if idErr != nil {
 		c.JSON(idErr.Status, idErr.Message)
@@ -45,7 +52,14 @@ func Get(c *gin.Context) {
 		c.JSON(err.Status, err)
 		return
 	}
-	c.JSON(http.StatusOK, user.Marshall(c.GetHeader("X-Public") == "true"))
+
+	// If the caller user id is the same as the user, display the full user.
+	fmt.Sprintf("Caller id: %v", oauth.GetCallerId(c.Request))
+	if oauth.GetCallerId(c.Request) == user.Id {
+		c.JSON(http.StatusOK, user.Marshall(false))
+		return
+	}
+	c.JSON(http.StatusOK, user.Marshall(oauth.IsPublic(c.Request)))
 }
 
 func Update(c *gin.Context) {
